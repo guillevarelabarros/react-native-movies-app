071357a2394f932e572ce968972afb5eed06b05d
Guille Varela Barros <guillevarelabarros@gmail.com>
2025-07-24 19:46:55 -0300

    L. 144. Navegación entre pantallas

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 // App.tsx
 
 import React from 'react';
-import { View, Text } from 'react-native';
+// import { View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { Navigation } from './presentation/navigation/Navigation';
 
 export const App = () => {
   return (
-    <View>
-      <Text>MoviesApp</Text>
-    </View>
+    <NavigationContainer>
+      <Navigation />
+    </NavigationContainer>
   );
 };
