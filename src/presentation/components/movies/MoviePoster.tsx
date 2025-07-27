3cd66802f8f32c6b1d28fab53007e39fab71fafd
Guille Varela Barros <guillevarelabarros@gmail.com>
2025-07-27 11:15:48 -0300

    159. Terminar carrusel y navegación

--- src/presentation/components/movies/MoviePoster.tsx
+++ src/presentation/components/movies/MoviePoster.tsx
@@ -3,14 +3,28 @@
 import React from 'react';
 import { View, Image, StyleSheet, Pressable } from 'react-native';
 import { Movie } from '../../../core/entities/movie.entity';
-
+import { NavigationProp, useNavigation } from '@react-navigation/native';
+import { RootStackParams } from '../../navigation/Navigation';
 interface Props {
   movie: Movie;
+  height?: number;
 }
 
-export const MoviePoster = ({ movie }: Props) => {
+export const MoviePoster = ({ movie, height = 420, width = 300 }: Props) => {
+  const navigation = useNavigation<NavigationProp<RootStackParams>>();
+
   return (
-    <Pressable>
+    <Pressable
+      onPress={() => navigation.navigate('Details', { movieId: movie.id })}
+      style={({ pressed }) => ({
+        width,
+        height,
+        marginHorizontal: 10,
+        paddingBottom: 20,
+        paddingHorizontal: 10,
+        opacity: pressed ? 0.9 : 1,
+      })}
+    >
       <View style={{ ...styles.imageContainer, width: 300, height: 400 }}>
         <Image style={styles.image} source={{ uri: movie.poster }} />
       </View>
