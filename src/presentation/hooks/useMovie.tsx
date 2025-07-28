14bdf2229ba5b8f7dbbe5495ed7acbb99c60a9c7
Guille Varela Barros <guillevarelabarros@gmail.com>
2025-07-28 10:07:20 -0300

    L. 163. Información de la película por ID

---
+++ src/presentation/hooks/useMovie.tsx
@@ -0,0 +1,17 @@
+// useMovie.tsx
+
+import { useEffect, useState } from 'react';
+
+export const useMovie = (movieId: number) => {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    loadMovie();
+  }, [movieId]);
+
+  const loadMovie = () => {};
+
+  return {
+    isLoading,
+  };
+};
--- src/presentation/screens/details/DetailsScreen.tsx
+++ src/presentation/screens/details/DetailsScreen.tsx
@@ -1,9 +1,18 @@
 // DetailsScreen.tsx
 
+import { StackScreenProps } from '@react-navigation/stack';
 import React from 'react';
 import { Text, View } from 'react-native';
+import { RootStackParams } from '../../navigation/Navigation';
+// import { useRoute } from '@react-navigation/native';
 
-export const DetailsScreen = () => {
+interface Props extends StackScreenProps<RootStackParams, 'Details'> {}
+
+export const DetailsScreen = ({ route }: Props) => {
+  const { movieId } = route.params;
+  console.log(movieId);
+  // const { movieId } = useRoute().params;
+
   return (
     <View>
       <Text>DetailsScreen</Text>
