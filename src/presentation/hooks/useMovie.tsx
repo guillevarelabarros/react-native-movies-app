318929b9f5ef05f8d542fe466e5c0c12e11e0101
Guille Varela Barros <guillevarelabarros@gmail.com>
2025-07-31 10:51:59 -0300

    L. 167. Estructuras de datos para los actores

--- src/presentation/hooks/useMovie.tsx
+++ src/presentation/hooks/useMovie.tsx
@@ -4,10 +4,12 @@ import { useEffect, useState } from 'react';
 import * as UseCases from '../../core/use-cases';
 import { movieDBFetcher } from '../../config/adapers/movieDB.adapter';
 import { FullMovie } from '../../core/entities/movie.entity';
+import { Cast } from '../../core/entities/cast.entity';
 
 export const useMovie = (movieId: number) => {
   const [isLoading, setIsLoading] = useState(true);
   const [movie, setMovie] = useState<FullMovie>();
+  const [cast, setCast] = useState<Cast[]>();
 
   useEffect(() => {
     loadMovie();
@@ -15,14 +17,27 @@ export const useMovie = (movieId: number) => {
 
   const loadMovie = async () => {
     setIsLoading(true);
-    const fullMovie = await UseCases.getMovieByUseCase(movieDBFetcher, movieId);
+    const fullMoviePromise = UseCases.getMovieByUseCase(
+      movieDBFetcher,
+      movieId,
+    );
+    const castPromise = UseCases.getMovieCastUseCase(movieDBFetcher, movieId);
+
+    const [fullMovie, cast] = await Promise.all([
+      fullMoviePromise,
+      castPromise,
+    ]);
+
     setMovie(fullMovie);
+    setCast(cast);
     setIsLoading(false);
-    console.log({ fullMovie });
+
+    console.log({ cast });
   };
 
   return {
     isLoading,
     movie,
+    cast,
   };
 };
