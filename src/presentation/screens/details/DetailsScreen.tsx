29f2a7d5683a8eeb80511e8ef6a1412dfb1b7fde
Guille Varela Barros <guillevarelabarros@gmail.com>
2025-07-31 07:19:04 -0300

    L. 165. Pantalla de detalles - Header

--- src/presentation/screens/details/DetailsScreen.tsx
+++ src/presentation/screens/details/DetailsScreen.tsx
@@ -4,7 +4,8 @@ import { StackScreenProps } from '@react-navigation/stack';
 import React from 'react';
 import { Text, View } from 'react-native';
 import { RootStackParams } from '../../navigation/Navigation';
-// import { useMovie } from '../../hooks/useMovie';
+import { MovieHeader } from '../../components/movie/MovieHeader';
+import { useMovie } from '../../hooks/useMovie';
 // import { useRoute } from '@react-navigation/native';
 
 interface Props extends StackScreenProps<RootStackParams, 'Details'> {}
@@ -12,11 +13,22 @@ interface Props extends StackScreenProps<RootStackParams, 'Details'> {}
 export const DetailsScreen = ({ route }: Props) => {
   const { movieId } = route.params;
   // const { movieId } = useRoute().params;
-  // const {} = useMovie{movieId};
+  const { isLoading, movie } = useMovie(movieId);
+
+  if (isLoading) {
+    return <Text>Loading</Text>;
+  }
 
   return (
     <View>
-      <Text>DetailsScreen</Text>
+      {/* Header */}
+      <MovieHeader
+        originalTitle={movie!.originalTitle}
+        title={movie!.title}
+        poster={movie!.poster}
+      />
+
+      {/* Details */}
     </View>
   );
 };
